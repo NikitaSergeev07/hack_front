4703ac78a29abdddecf5dc6901ce1dd6f01f5d61
Idzanaagi <anmorgunowa@gmail.com>
2023-01-18 01:46:48 +0400

    feat: add EnterScreen, normalize, scss

---
+++ src/components/EnterScreen/EnterScreen.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="enter-screen wrapper">
+        <h1 class="headline enter-screen__headline"> Ready for a gteat <br> User experience?</h1>
+        <p class="subtitle enter-screen__subtitle">Bring your media to the next level!</p>
+
+        <div class="enter-screen__links">
+            <button onclick="document.location='#/sign-up'" class="enter-screen__link-signup">Sign Up</button>
+            <div class="enter-screen__link-signin">
+                <a href="#/sign-in"> <i class="bi bi-arrow-right"></i> </a>
+            </div>
+        </div>
+
+        <div class="enter-screen__image">
+            <img src="../../assets/woman.svg" alt="" />
+        </div>
+    </div>
+</template>
+
+<style lang="scss" src="./enter-screen.scss"></style>
