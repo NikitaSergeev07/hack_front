1f809f26268dcb27acb89352280d2e68a0693ed7
Idzanaagi <anmorgunowa@gmail.com>
2023-01-18 22:24:07 +0400

    feat: add routing

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template>
-  <EnterScreen />
-  <SignIn />
+  <!-- <EnterScreen /> -->
+  <!-- <SignIn />
   <SignUp />
-  <MainPage />
+  <MainPage /> -->
+  <router-view> </router-view>
 </template>
 
 <script>
-import EnterScreen from './components/EnterScreen/EnterScreen.vue';
-import MainPage from './components/MainPage/MainPage.vue';
-import SignIn from './components/SignIn/SignIn.vue';
-import SignUp from './components/SignUp/SignUp.vue';
+// import EnterScreen from './components/EnterScreen/EnterScreen.vue';
+// import MainPage from './components/MainPage/MainPage.vue';
+// import SignIn from './components/SignIn/SignIn.vue';
+// import SignUp from './components/SignUp/SignUp.vue';
 
 export default {
   name: 'App',
   components: {
-    EnterScreen,
-    SignIn,
-    SignUp,
-    MainPage,
+    // EnterScreen,
+    // SignIn,
+    // SignUp,
+    // MainPage,
   }
 }
 </script>
